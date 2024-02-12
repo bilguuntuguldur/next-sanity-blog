b85987638f6cdff6b0362aeda59bc0146fce1ab9
bilguutuguldur <gbaldan040@gmail.com>
2024-02-12 23:40:02 +0800

    Change

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import '../globals.css';
-import Header from "@/components/header/header";
-import Footer from '@/components/footer/footer';
+import Header from "@/components/header/Header";
+import Footer from '@/components/footer/Footer';
 import { ThemeProvider } from '@/context/ThemeContext';
 
 export const metadata = {
